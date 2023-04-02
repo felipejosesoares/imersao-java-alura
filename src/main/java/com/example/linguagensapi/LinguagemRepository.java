bfad3e6b5c3145ca48debdd237f69b1cf4588001
FeliipeJ <felipejose.s1994@gmail.com>
2023-04-02 08:00:03 -0300

    Add existing file

---
+++ src/main/java/com/example/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,7 @@
+package com.example.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+
+}
